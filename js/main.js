0ff17f1b08c77538978166840c89d63350884647
Malcolm Grace <malcolmgrace.digital@gmail.com>
2026-02-13 21:55:15 -0500

    Add testimonial slider and centralize site components
    - Customer review carousel with auto-rotation
    - Refactored header/nav/footer into reusable components

--- js/main.js
+++ js/main.js
@@ -63,18 +63,28 @@
     // Testimonials carousel
     $(".testimonial-carousel").owlCarousel({
         autoplay: true,
+        autoplayTimeout: 5000,
+        autoplayHoverPause: true,
         smartSpeed: 1000,
-        items: 1,
-        dots: false,
+        center: false,
+        dots: true,
         loop: true,
-        nav: true,
+        margin: 25,
+        nav : true,
         navText : [
-            '<i class="bi bi-chevron-left"></i>',
-            '<i class="bi bi-chevron-right"></i>'
-        ]
+            '<i class="bi bi-arrow-left"></i>',
+            '<i class="bi bi-arrow-right"></i>'
+        ],
+        responsive: {
+            0:{
+                items:1
+            },
+            768:{
+                items:2
+            }
+        }
     });
 
     document.getElementById('year').textContent = new Date().getFullYear();
     
 })(jQuery);
-
