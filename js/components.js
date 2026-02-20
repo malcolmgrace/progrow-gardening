e89888dfc5b3b361d8ee405e342238b9e1f1695c
Malcolm Grace <malcolmgrace.digital@gmail.com>
2026-02-19 21:24:00 -0500

    Address change

--- js/components.js
+++ js/components.js
@@ -8,7 +8,7 @@ const siteData = {
     // Contact Information
     phone: '(249) 878 9140',
     email: 'progrowgardening@gmail.com',
-    address: '152 Sunita Crescent, Nepean, ON K2J 5S8',
+    address: '367 Bankside Way, Nepean, ON K2J 6K4',
     
     // Social Media Links
     facebook: 'https://www.facebook.com/profile.php?id=61574774193213',
