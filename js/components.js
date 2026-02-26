5acd4760ed66362d22298d1fcbea18e051d65a90
Malcolm Grace <malcolmgrace.digital@gmail.com>
2026-02-25 20:18:45 -0500

    fix(components): remove mt-5 from footer to eliminate whitespace above it
    - Footer mt-5 caused white gap between .quote section and footer on index.html
    - Changed to mt-0; section padding above footer is sufficient

--- js/components.js
+++ js/components.js
@@ -90,7 +90,7 @@ function renderFooter() {
     const currentYear = new Date().getFullYear();
     
     return `
-    <div class="container-fluid bg-dark text-light footer mt-5 py-5 wow fadeIn" data-wow-delay="0.1s">
+    <div class="container-fluid bg-dark text-light footer mt-0 py-5 wow fadeIn" data-wow-delay="0.1s">
         <div class="container py-5">
             <div class="row g-5">
                 <div class="col-lg-4 col-md-6">
